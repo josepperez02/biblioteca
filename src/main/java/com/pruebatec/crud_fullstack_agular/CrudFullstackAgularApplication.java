c503e6e071db911dc92e39d90a42251c51be3943
josepperez02 <poloerezjose@gmail.com>
2025-07-10 17:16:01 -0500

    primer commit, creacion de proyecto y conexion a mysql

---
+++ src/main/java/com/pruebatec/crud_fullstack_agular/CrudFullstackAgularApplication.java
@@ -0,0 +1,13 @@
+package com.pruebatec.crud_fullstack_agular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFullstackAgularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFullstackAgularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebatec/crud_fullstack_agular/CrudFullstackAgularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebatec.crud_fullstack_agular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFullstackAgularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
